56308ea0348652d95dd2eb64e51ef6642a4e90af
Walysson Silveira <walysson_lopes0@gmail.com>
2020-09-10 11:57:41 -0300

    Correção geral do trabalho antes da primeira apresentação

--- cadastro_usuario/js/functions_cadastro.js
+++ cadastro_usuario/js/functions_cadastro.js
@@ -1,25 +1,33 @@
 var campo_user = document.getElementById("user");
-var campo_senha= document.getElementById("senha");
+var campo_senha = document.getElementById("senha");
 var campo_inst = document.getElementById("inst");
 var botao_login = document.getElementById("login");
 var botao_cadastro = document.getElementById("cadastrar");
 
 myStorage = window.localStorage
-
-botao_cadastro.onclick = function cadastro(event)
-{ 
-    event.preventDefault();
-    if(campo_user.value.length > 5 && campo_senha.value.length > 7 && campo_inst.value.length > 0){
-        if(myStorage.getItem(campo_user.value) == null){
-          myStorage.setItem(campo_user.value,campo_senha.value, campo_inst.value)
+botao_cadastro.onclick = function cadastro(event) {
+  event.preventDefault();
+  if (campo_user.value.length > 5) {
+    if (campo_senha.value.length > 7) {
+      if (campo_inst.value.length > 0) {
+        if (myStorage.getItem(campo_user.value) == null) {
+          myStorage.setItem(campo_user.value, campo_senha.value, campo_inst.value)
           alert("Cadastro feito com sucesso. Clique aqui para voltar para a tela de Login")
           location.href = "login.html";
         }
-        else{
-            alert("Nome de usuário ja está sendo utilizado")
+        else {
+          alert("Nome de usuário ja está sendo utilizado")
         }
+      }
+      else {
+        alert("Você deve colocar uma instituição de ensino")
+      }
+    }
+    else {
+      alert("Sua senha deve conter no mínimo 8 caracteres ")
+    }
   }
-  else{
-      alert("Nome de usuário deve conter ")
+  else {
+    alert("Nome de usuário deve conter no mínimo 6 caracteres ")
   }
 }
--- exibir_entidade/JS/functions_exibir_entidade.js
+++ exibir_entidade/JS/functions_exibir_entidade.js
@@ -7,21 +7,24 @@ var id = 0;
 var corpo_tabela = document.querySelector("tbody[id='tbodyP'");
 var template = document.querySelector("#template1");
 //Primeira linha do localstorage é um header contendo a sequência de parâmetros
-myStorage.setItem("Linha 1 - Header", "ID do Usuário, Nome do Usuário, ID do Usuário, Instituição")
+myStorage.setItem("Linha 1 - Header", "ID do Usuário, Nome do Usuário,Título do Projeto, Instituição")
 
 //Entidade das pessoas
-function Pessoa(nome, instituicao, projetos) {
+function Pessoa(nome, instituicao, projetos, curso) {
     this.nome = nome;
     this.id = "" + id++;
     this.instituicao = instituicao;
     this.projetos = projetos;
+    this.curso = curso;
 
 };
 //Entidade dos projeto
-function Projeto(autor, titulo, descricao) {
+function Projeto(autor, titulo, descricao, palavrasChaves, tipo) {
     this.titulo = titulo;
     this.autor = autor;
     this.descricao = descricao;
+    this.palavrasChaves = palavrasChaves;
+    this.tipo = tipo;
 }
 //Entidade que possui um vetor de pessoas que contém todas as pessoas que possuem projetos.
 function entidades(pessoas) {
@@ -31,13 +34,13 @@ function entidades(pessoas) {
         for (var i = 0; i < this.pessoas.length; i++) {
             for (var j = 0; j < this.pessoas[i].projetos.length; j++) {
                 lista_td = template.content.querySelectorAll("td");
-                lista_td[0].textContent = this.pessoas[i].nome
-                lista_td[1].textContent = this.pessoas[i].projetos[j].titulo
+                lista_td[0].textContent = this.pessoas[i].projetos[j].tipo
+                lista_td[1].textContent = this.pessoas[i].projetos[j].autor
                 lista_td[2].textContent = this.pessoas[i].instituicao
                 var nova_linha = document.importNode(template.content, true)
                 corpo_tabela.appendChild(nova_linha);
                 //console.log("Na posição " + i + " " + this.projetos[i].titulo)
-                info = [this.pessoas[i].id, this.pessoas[i].nome, this.pessoas[i].instituicao]
+                info = [this.pessoas[i].id, this.pessoas[i].nome, this.pessoas[i].instituicao,this.pessoas[i].projetos[j].titulo]
                 myStorage.setItem("Título do Projeto -> " + this.pessoas[i].projetos[j].titulo, JSON.stringify(info))
             }
         }
@@ -45,20 +48,17 @@ function entidades(pessoas) {
 }
 //Mostrando entidades de forma dinâmica
 function mostrarEntidades() {
-    var pj1 = new Projeto("Almada", "NPDS", "Trabalho sobre o NPDS")
-    var pj2 = new Projeto("Walysson", "BIA", "Trabalho sobre o BIA")
-    var pj3 = new Projeto("Walysson", "WEB", "Trabalho sobre o WEB")
-    var pj4 = new Projeto("Maria", "PID", "Trabalho sobre o PID")
+    var pj1 = new Projeto("Ana Larissa de Queiroz França", "Estudo a cerca da Periodontite","Efeito de segurança de 6,7-dimetoxi-3-nitrocumarina na Periodontite", "saúde,periodontite","Conclusão de Curso")
+    var pj2 = new Projeto("Maria Clarice Sousa", "Estágio no NPDS", "Trabalho sobre o NPDS","NPDS, desenvolvendo","Descrição de estágio - NPDS")
+    var pj3 = new Projeto("Maria Clarice Sousa", "PID - Matemática", "Trabalho sobre o PID de matemática 1","PID, aulas, matemática","Descrição de bolsa - PID")
 
     var projetos1 = [pj1];
-    var projetos2 = [pj4];
-    var projetosW = [pj2, pj3];
+    var projetos2 = [pj2,pj3];
 
-    var p1 = new Pessoa("Almada", "UFC", projetos1)
-    var p2 = new Pessoa("Walysson", "UECE", projetosW)
-    var p3 = new Pessoa("Maria", "IFCE", projetos2)
+    var p1 = new Pessoa("Ana Larissa de Queiroz França", "Universidade Federal do Ceará (UFC)", projetos1, "Ciências Biológicas")
+    var p2 = new Pessoa("Maria Clarice Sousa", "Instituto Federal do Ceará (IFCE)", projetos2, "Matemática")
 
-    var pessoas = [p1, p2, p3]
+    var pessoas = [p1, p2]
     var ent = new entidades(pessoas);
     ent.criar_tabela_template();
 }
