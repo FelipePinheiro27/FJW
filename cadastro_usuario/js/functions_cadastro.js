57d90a00c8428461026d36be790cf09606bed99d
FelipePinheiro27 <felipe.afp27@gmail.com>
2020-09-09 22:24:29 -0300

    Cadastro de instituicao

--- cadastro_usuario/js/functions_cadastro.js
+++ cadastro_usuario/js/functions_cadastro.js
@@ -1,5 +1,6 @@
 var campo_user = document.getElementById("user");
 var campo_senha= document.getElementById("senha");
+var campo_inst = document.getElementById("inst");
 var botao_login = document.getElementById("login");
 var botao_cadastro = document.getElementById("cadastrar");
 
@@ -8,9 +9,9 @@ myStorage = window.localStorage
 botao_cadastro.onclick = function cadastro(event)
 { 
     event.preventDefault();
-    if(campo_user.value.length > 5 && campo_senha.value.length > 7){
+    if(campo_user.value.length > 5 && campo_senha.value.length > 7 && campo_inst.value.length > 0){
         if(myStorage.getItem(campo_user.value) == null){
-          myStorage.setItem(campo_user.value,campo_senha.value)
+          myStorage.setItem(campo_user.value,campo_senha.value, campo_inst.value)
           alert("Cadastro feito com sucesso. Clique aqui para voltar para a tela de Login")
           location.href = "login.html";
         }
