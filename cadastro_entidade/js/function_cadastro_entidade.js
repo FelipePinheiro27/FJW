9c6f5923ba355a312ce6c6d8bc294f071b8135ea
FelipePinheiro27 <felipe.afp27@gmail.com>
2020-09-10 13:35:32 -0300

    Ajustes cadastro_entidade

--- cadastro_entidade/js/function_cadastro_entidade.js
+++ cadastro_entidade/js/function_cadastro_entidade.js
@@ -2,13 +2,18 @@ function buttonClear(){
     document.getElementById("titulo").value = "";
     document.getElementById("desc").value = "";
     document.getElementById("ficheiro").value = null;
+    document.getElementById("pChaves").value = "";
+
+
 }
 
 function adicionar(){
     var titulo = document.getElementById("titulo").value;
     var desc = document.getElementById("desc").value;
     var file = document.getElementById("ficheiro").value;
-    var obj = {titulo:titulo, descricao:desc, ficheiro:file};
+    var pChaves = document.getElementById("pChaves").value;
+    var opc = document.getElementById("opc").value;
+    var obj = {titulo:titulo, descricao:desc, ficheiro:file, chaves:pChaves, tipo:opc};
 
     var preenchido = validacao();
     if(preenchido == true){
@@ -20,7 +25,7 @@ function adicionar(){
 
 function validacao(){
     if(document.getElementById("titulo").value != "" && document.getElementById("desc").value != ""
-    && document.getElementById("ficheiro").value != ""){
+    && document.getElementById("ficheiro").value != "" && document.getElementById("pChaves").value != ""){
         return true;
     }
     else
